ac7d843462d0f2c455ddfa5b5808725308473fd3
gaoyang <iqqgy@GY-PC>
2025-03-19 14:49:57 +0800

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xtquant',
+    version='xtquant_241014	',
+    description='QMT xtquant library',
+    author='albert',
+    author_email='iqqg@outlook.com',
+    packages=find_packages(),
+    install_requires=[],
+)
