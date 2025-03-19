e24b2d83cf14348fe393a40ad9bae8fba686d2fd
gaoyang <iqqgy@GY-PC>
2025-03-19 17:06:52 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,34 @@ from setuptools import setup, find_packages
 setup(
     name='xtquant',
     version='24.10.14',
-    # py_modules=['xttools', 'xtdata', 'xttrader', 'xttype', 'xtutil', 'xtconstant', 'xtconn', 'xtdata_config', 'xtdatacenter', 'xtextend', 'xtview'],
-    # package_dir={'': '.'},
-    # packages=['xtquant', 'xtbson', 'qmttools', 'metatable'],
-    packages=find_packages(),
-    description='QMT xtquant library',
     author='albert',
     author_email='iqqg@outlook.com',
-    install_requires=[],
+    description='QMT xtquant library',
+    packages=find_packages(include=['xtquant', 'xtquant.*']),
+    package_data={
+        'xtquant': [
+            '*.pyd',
+            '*.dll',
+            '*.ini',
+            '*.log4cxx',
+            # 如果你需要包含子目录中的文件，可以进一步指定，例如：
+            # 'config/*',
+            # 'doc/*',
+            # 'metatable/*',
+            # 'qmttools/*',
+            # 'xtbson/*'
+        ]
+    },
+    install_requires=[
+        # 列出该包依赖的其他 Python 包，例如：
+        # 'numpy',
+        # 'pandas'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.6',
 )
+    
